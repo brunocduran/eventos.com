5f5888c5565ad7c4204eaa0fd284679c9c796a29
Igor Buosi <igorbiancobuosi@hotmail.com>
2022-10-25 22:20:03 -0300

    teste123

---
+++ src/java/br/com/evento/utils/Teste123.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.evento.utils;
+
+/**
+ *
+ * @author igorb
+ */
+public class Teste123 {
+    
+}
